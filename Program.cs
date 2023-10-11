dd45a34cbd15daf76ffc30d23bba4e261db5f881
Nara Lice <naralice64@gmail.com>
2023-10-10 21:09:27 -0300

    Desafio concluído!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "NokiaG60", imei: "112233", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone");
+Smartphone iphone = new Iphone(numero: "789101", modelo: "Iphone14", imei: "445566", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
